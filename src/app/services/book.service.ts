bdc92376be0aae6bd345223fdf5fd4daab470e30
Samir Tapiero <samir.hernando@wolox.co>
2020-05-04 19:51:32 -0500

    Add book service, add token interceptor

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -4,13 +4,23 @@ import { Observable, throwError } from 'rxjs';
 import { environment } from '@env/environment';
 import { catchError } from 'rxjs/operators';
 
+<<<<<<< HEAD
 @Injectable()
+=======
+@Injectable({
+  providedIn: 'root'
+})
+>>>>>>> Add book service, add token interceptor
 export class BookService {
   private apiUrl = `${environment.apiUrl}/books`;
 
   constructor(private http: HttpClient) { }
 
+<<<<<<< HEAD
   getBooks(): Observable<any> {
+=======
+  getBooks (): Observable<any> {
+>>>>>>> Add book service, add token interceptor
     return this.http.get<any>(
       this.apiUrl,
       ).pipe(
