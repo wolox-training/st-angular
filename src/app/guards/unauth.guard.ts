f6d77423ced0bd1f32e2db35d0a5d27124145fd1
Samir Tapiero <samir.hernando@wolox.co>
2020-05-06 12:24:24 -0500

    Remove unused code

--- src/app/guards/unauth.guard.ts
+++ src/app/guards/unauth.guard.ts
@@ -2,9 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { UserService } from '@app/services/user.service';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class UnauthGuard implements CanActivate {
 
   constructor(private router: Router, private userService: UserService) {}
