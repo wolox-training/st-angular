74b3518ab5028f909917d85689311fad42b55a5e
Samir Tapiero <samir.hernando@wolox.co>
2020-05-08 12:11:28 -0500

    Add routing files in modules

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,24 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthComponent } from './auth.component';
-import { Routes, RouterModule } from '@angular/router';
-import { BookListComponent } from '@app/screens/book-list/book-list.component';
 import { AuthGuard } from '@app/guards/auth.guard';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: AuthComponent,
-    children: [
-      { path: 'books', component: BookListComponent }
-    ]
-  }
-];
-
+import { AuthRoutingModule } from './auth-routing.module';
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    AuthRoutingModule
   ],
   providers: [AuthGuard]
 })
